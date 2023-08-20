de054157a528adeb48bf569a222ab61ced09eba7
Thomas Sarfo <tomsarfodavis@gmail.com>
2023-08-20 16:20:44 +0000

    decorate drug detail table with css

--- stock-postion/src/DrugDetails.jsx
+++ stock-postion/src/DrugDetails.jsx
@@ -1,3 +1,55 @@
+import { Chip } from "@mui/material";
+import "./App.css";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import BoltIcon from "@mui/icons-material/Bolt";
 export default function DrugDetails({ ExpandDetail }) {
-	return <div>{ExpandDetail === true && <div>drug details</div>}</div>;
+	return (
+		<div>
+			{ExpandDetail === true && (
+				<table width={450} className="DrugDetailTable">
+					<tr>
+						<td>
+							<b>Drug details</b>
+						</td>
+						<td align="right">
+							<Chip
+								icon={<BoltIcon />}
+								label="Movable"
+								variant="contained"
+								size="small"
+								sx={{
+									background: "#ff9a02",
+									color: "white",
+								}}
+							/>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<b>Availability</b>
+						</td>
+						<td align="center">
+							<b>345pks</b>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<b>Old Price</b>
+						</td>
+						<td align="center">
+							<b>¢19</b>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<b>Metformin Syrup (Alternative)</b>
+						</td>
+						<td align="center">
+							<b>¢21</b>
+						</td>
+					</tr>
+				</table>
+			)}
+		</div>
+	);
 }
